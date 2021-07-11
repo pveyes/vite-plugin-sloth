5e539c50b23929a9389b2b21e40519e2ca81399d
Fatih Kalifa <fatihkalifa@gmail.com>
2021-07-11 09:54:18 +0700

    initial support for recursive import (#3)

--- dev-runtime.js
+++ dev-runtime.js
@@ -1,5 +1,10 @@
 /// <reference types="./sloth" />
 // @ts-check
+
+/**
+ * @typedef {{ name: string, incoming: Record<string, Vertex>, incomingNames: string[], value: string }} Vertex
+ */
+
 /**
  * @type {Map<string, DocumentFragment>}
  */
@@ -8,19 +13,17 @@ const templateCache = new Map();
 // @ts-ignore: property .hot is not available on ImportMeta
 const hmr = import.meta.hot;
 
+const ROOT_VERTEX_NAME = "root";
+
 if (hmr) {
   hmr.on("template-update", async (data) => {
-    const segment = data.path.split("/");
-    const matchingLink = Array.from(document.querySelectorAll("link")).find(
-      (link) =>
-        link.getAttribute("href").endsWith(`${segment[segment.length - 1]}`)
-    );
+    const href = deps.names.find((name) => data.path.endsWith(name));
 
-    if (!matchingLink) {
+    if (!href) {
       return;
     }
 
-    const templateHTML = await fetch(matchingLink.href, {
+    const templateHTML = await fetch(href, {
       headers: {
         pragma: "no-cache",
         "cache-control": "no-cache",
@@ -28,34 +31,41 @@ if (hmr) {
     }).then((res) => res.text());
     const div = document.createElement("div");
     div.innerHTML = templateHTML;
-    const t = div.querySelector("template");
-    replaceCustomElement(t.id, t.content);
-    // perform custom update
+    const template = div.querySelector("template");
+
+    /** @type {Array<Document|ShadowRoot>} */
+    let roots = [document];
+    visitDAG(deps.add(href), (vertex) => {
+      if (vertex.name !== ROOT_VERTEX_NAME && vertex.name !== href) {
+        roots = roots.flatMap((root) => {
+          return Array.from(
+            /** @type {NodeListOf<Element>} */ (
+              root.querySelectorAll(`${vertex.value},[is="${vertex.value}"]`)
+            )
+          ).map((el) => el.shadowRoot);
+        });
+      }
+    });
+
+    templateCache.set(template.id, template.content);
+
+    roots.forEach((root) => {
+      replaceCustomElement(template.id, template.content, root);
+    });
   });
 
   // Setup custom elements from external templates.
   // Assign the promise so we can wait if some polymorphic elements
   // uses external template
-  const externalTemplatesReady = Promise.all(
-    Array.from(document.querySelectorAll("link"))
-      .filter((link) => link.rel === "import" && link.href.endsWith("html"))
-      .map(async (link) => {
-        const templateHTML = await fetch(link.href).then((res) => res.text());
-        const div = document.createElement("div");
-        div.innerHTML = templateHTML;
-        const t = div.querySelector("template");
-
-        if (!t) {
-          throw new Error(`Template from ${link.href} not found`);
-        }
-
-        initCustomElement(t.id, t.content);
-      })
+  const externalTemplatesReady = fetchExternalDependencies(
+    Array.from(document.querySelectorAll("link")).filter(
+      (link) => link.rel === "import" && link.href.endsWith("html")
+    )
   );
 
   // as well as inline templates
-  document.querySelectorAll("template").forEach((t) => {
-    initCustomElement(t.id, t.content);
+  document.querySelectorAll("template").forEach((template) => {
+    initCustomElement(template.id, template.content);
   });
 
   // upgrade polymorphic elements to custom element
@@ -64,45 +74,88 @@ if (hmr) {
   ).forEach(async (el) => {
     const name = el.getAttribute("is");
 
-    let t = templateCache.get(name);
-    if (!t) {
+    let template = templateCache.get(name);
+    if (!template) {
       // only wait for external templates if it doesn't exist in the cache
       await externalTemplatesReady;
-      t = templateCache.get(name);
+      template = templateCache.get(name);
 
       // by now, template must be resolved or is not defined everywhere
-      if (!t) {
+      if (!template) {
         console.error(
-          `[sloth] Template ${name} not found for polymorphic element`,
+          `[sloth] Template \`${name}\` not found for polymorphic element`,
           { el }
         );
       }
     }
 
-    renderPolymorphicElement(el, name, t);
+    renderPolymorphicElement(el, name, template);
   });
 }
 
 /**
- * @param {string} name
- * @param {DocumentFragment} t
+ * @param {HTMLLinkElement[]} paths
+ * @param {string?} root
+ * @param {string?} fromName
  */
-function initCustomElement(name, t) {
-  templateCache.set(name, t);
-  customElements.define(name, createCustomElement(name, t));
+function fetchExternalDependencies(
+  paths,
+  root = "",
+  fromName = ROOT_VERTEX_NAME
+) {
+  return Promise.all(
+    paths.map(async (link) => {
+      const path = link.href;
+      const url = new URL(path);
+      const target = root + url.pathname;
+      const templateHTML = await fetch(target).then((res) => res.text());
+      const div = document.createElement("div");
+      div.innerHTML = templateHTML;
+      const template = div.querySelector("template");
+
+      if (!template) {
+        throw new Error(`Template from ${path} not found`);
+      }
+
+      deps.addVertex(target, template.id);
+      deps.addEdge(fromName, target);
+
+      /**
+       * @type {Array<HTMLLinkElement>}
+       */
+      const imports = Array.from(div.querySelectorAll("link[rel=import]"));
+      if (imports.length === 0) {
+        initCustomElement(template.id, template.content);
+        return;
+      }
+
+      const nextRoot = target.split("/").slice(0, -1).join("/");
+      await fetchExternalDependencies(imports, nextRoot + root, target);
+      initCustomElement(template.id, template.content);
+    })
+  );
 }
 
 /**
- * @param {string} name
- * @param {DocumentFragment} t
+ * @param {string} templateId
+ * @param {DocumentFragment} fragment
  */
-function replaceCustomElement(name, t) {
-  templateCache.set(name, t);
-  const clazz = createCustomElement(name, t);
+function initCustomElement(templateId, fragment) {
+  templateCache.set(templateId, fragment);
+  customElements.define(templateId, createCustomElement(templateId, fragment));
+}
+
+/**
+ * @param {string} templateId
+ * @param {DocumentFragment} fragment
+ * @param {Document|ShadowRoot} root
+ */
+function replaceCustomElement(templateId, fragment, root) {
+  const clazz = createCustomElement(templateId, fragment);
 
   // refresh custom elements
   /** @type {NodeListOf<HTMLElement>} */ (
-    document.querySelectorAll(name)
+    root.querySelectorAll(templateId)
   ).forEach((node) => {
     node.disconnectedCallback();
     Object.setPrototypeOf(node, clazz.prototype);
@@ -111,41 +164,45 @@ function replaceCustomElement(name, t) {
 
   // refresh polymorphic elements
   /** @type {NodeListOf<PolymorphicElement>} */ (
-    document.querySelectorAll(`[is="${name}"]`)
+    root.querySelectorAll(`[is="${templateId}"]`)
   ).forEach((node) => {
     Object.setPrototypeOf(node, clazz.prototype);
-    renderPolymorphicElement(node, name, t);
+    renderPolymorphicElement(node, templateId, fragment);
   });
 }
 
 /**
  * @param {PolymorphicElement} el
- * @param {string} name
- * @param {DocumentFragment} t
+ * @param {string} templateId
+ * @param {DocumentFragment} fragment
  */
-function renderPolymorphicElement(el, name, t) {
+function renderPolymorphicElement(el, templateId, fragment) {
   if (el.shadowRoot) {
     // re-renders
-    el.shadowRoot.replaceChild(t.cloneNode(true), el.shadowRoot.children[0]);
-    bindVariables(el.shadowRoot, el, name);
+    el.shadowRoot.replaceChild(
+      fragment.cloneNode(true),
+      el.shadowRoot.children[0]
+    );
+    bindVariables(el.shadowRoot, el, templateId);
     return;
   }
 
   const root = el.attachShadow({ mode: "open" });
-  root.appendChild(t.cloneNode(true));
+  root.appendChild(fragment.cloneNode(true));
 
-  bindVariables(root, el, name);
+  bindVariables(root, el, templateId);
 
+  injectGlobalStyle(root);
   window.addEventListener("load", () => {
     injectGlobalStyle(root);
   });
 }
 
 /**
- * @param {string} name
- * @param {DocumentFragment} t
+ * @param {string} templateId
+ * @param {DocumentFragment} fragment
  */
-function createCustomElement(name, t) {
+function createCustomElement(templateId, fragment) {
   return class extends HTMLElement {
     constructor() {
       super();
@@ -169,10 +226,10 @@ function createCustomElement(name, t) {
 
     render() {
       const wrapper = document.createElement("div");
-      wrapper.setAttribute("data-template", name);
-      wrapper.appendChild(t.cloneNode(true));
+      wrapper.setAttribute("data-template", templateId);
+      wrapper.appendChild(fragment.cloneNode(true));
 
-      bindVariables(wrapper, this, name);
+      bindVariables(wrapper, this, templateId);
 
       if (this.root.children.length === 0) {
         this.root.appendChild(wrapper);
@@ -237,3 +294,108 @@ function injectGlobalStyle(root) {
     root.adoptedStyleSheets = [sheet];
   }
 }
+
+/**
+ * Dependency Graph represented as DAG
+ */
+class DependencyGraph {
+  constructor() {
+    /**
+     * @type {string[]}
+     * @public
+     */
+    this.names = [];
+    /**
+     * @type {Record<string,Vertex>}
+     * @private
+     */
+    this.vertices = Object.create(null);
+  }
+
+  /**
+   * @param {string} name
+   * @return {Vertex}
+   */
+  add(name) {
+    if (this.vertices[name]) {
+      return this.vertices[name];
+    }
+
+    const vertex = Object.assign(Object.create(null), {
+      name,
+      incoming: Object.create(null),
+      incomingNames: [],
+      value: null,
+    });
+
+    this.vertices[name] = vertex;
+    this.names.push(name);
+    return vertex;
+  }
+
+  /**
+   * @param {string} name
+   * @param {string} value
+   * @return {Vertex}
+   */
+  addVertex(name, value) {
+    const vertex = this.add(name);
+    vertex.value = value;
+    return vertex;
+  }
+
+  /**
+   * @param {string} fromName
+   * @param {string} toName
+   * @return {void}
+   */
+  addEdge(fromName, toName) {
+    const from = this.add(fromName);
+    const to = this.add(toName);
+
+    if (to.incoming[fromName]) {
+      return;
+    }
+
+    visitDAG(from, (vertex, path) => {
+      if (vertex.name === toName) {
+        throw new Error(
+          `Circular dependencies detected: ${toName} <- ${path.join(" <- ")}`
+        );
+      }
+    });
+
+    to.incoming[fromName] = from;
+    to.incomingNames.push(fromName);
+  }
+}
+
+/**
+ * @callback visitCallback
+ * @param {Vertex} vertex
+ * @param {string[]?} path
+ */
+
+/**
+ *
+ * @param {Vertex} vertex
+ * @param {visitCallback} cb
+ */
+function visitDAG(vertex, cb, visited = {}, path = []) {
+  const { name, incoming, incomingNames } = vertex;
+  if (visited[name]) {
+    return;
+  }
+
+  path.push(name);
+  visited[name] = true;
+
+  incomingNames.forEach((name) => {
+    visitDAG(incoming[name], cb, visited, path);
+  });
+
+  cb(vertex, path);
+  path.pop();
+}
+
+const deps = new DependencyGraph();
